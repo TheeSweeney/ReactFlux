c7b8bbe41b940beca1ecf5620d5c5c1e012418ec
Brian Sweeney <bsweeney930@gmail.com>
2017-04-18 10:55:27 -0400

    wrote fetch basic fetch functino

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,3 +1,15 @@
 var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = require('../../config.js').API_KEY
+
+
+module.exports = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID' + apiKey
+      }
+    });
+  }
+};
+
