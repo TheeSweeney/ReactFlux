34a6d8e482191553debf1184917d06a030e7a378
Brian Sweeney <bsweeney930@gmail.com>
2017-04-18 11:11:33 -0400

    created topiclist component and connected to main

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -12,9 +12,7 @@ module.exports = window.api = {
     })
     .then(function(response){
       return response.json();
-    }).then(function(data){
-      console.log(data)
-    });
+    })
   }
 };
 
