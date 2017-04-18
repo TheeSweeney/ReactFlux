08bfb54ce4755eb65c81a28b7f74cfc15f295227
Brian Sweeney <bsweeney930@gmail.com>
2017-04-18 11:30:44 -0400

    rendering topics onto page

--- src/components/topicList.jsx
+++ src/components/topicList.jsx
@@ -1,11 +1,38 @@
 var React = require('react');
+var Api = require('../utils/api');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      topics: ['butt']
+    }
+  },
+
+  componentWillMount: function() {
+    Api.get('topics/defaults')
+      .then(function(data){
+        this.setState({
+          topics: data.data
+        })
+      }.bind(this));
+  },
+
   render: function() {
     return(
       <div className="list-group">
         Topic List
+        {this.renderTopics()}
       </div>
     )
+  },
+
+  renderTopics: function() {
+    return this.state.topics.map(function(topic){
+      return (
+        <li>
+          {topic.name}
+        </li>
+      )
+    })
   }
 })
