0674ab9e6e4b2384307e52dcf82b5f9c1e08916c
Brian Sweeney <bsweeney930@gmail.com>
2017-04-18 11:03:25 -0400

    fetch testing

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -3,12 +3,17 @@ var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = require('../../config.js').API_KEY
 
 
-module.exports = {
+module.exports = window.api = {
   get: function(url) {
     return fetch(rootUrl + url, {
       headers: {
         'Authorization': 'Client-ID' + apiKey
       }
+    })
+    .then(function(response){
+      return response.json();
+    }).then(function(data){
+      console.log(data)
     });
   }
 };
